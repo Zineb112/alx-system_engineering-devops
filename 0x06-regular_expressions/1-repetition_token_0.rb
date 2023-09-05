6c7e13557c74a5176e78e4a326d8c33be2f03800
Zineb112 <zbelassiri@gmail.con>
2023-09-05 13:25:34 -0700

    1-repetition_token_0.rb added

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -12,12 +12,12 @@ input_string = ARGV[0]
 # Define the regular expression pattern for matching "School"
 regex_pattern = /School/
 
-# Use the match method to find the first occurrence of the pattern in the input string
+# Use the match method to find a match in the input string
 match = input_string.match(regex_pattern)
 
 # Check if a match was found and print the result
 if match
   puts match[0]
 else
-  puts "No match found"
+  puts "No match found."
 end
