39fc2fcd239d26bf705fc4c43ecec1621e5df92e
Zineb112 <zbelassiri@gmail.con>
2023-09-05 13:22:25 -0700

    1-repetition_token_0.rb added

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <input_string>"
+  exit(1)
+end
+
+# Extract the input string from command-line arguments
+input_string = ARGV[0]
+
+# Define the regular expression pattern for matching "School"
+regex_pattern = /School/
+
+# Use the match method to find the first occurrence of the pattern in the input string
+match = input_string.match(regex_pattern)
+
+# Check if a match was found and print the result
+if match
+  puts match[0]
+else
+  puts "No match found"
+end
