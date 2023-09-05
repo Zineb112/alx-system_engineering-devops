1a5b38a4aab1243e16f4d40f98b9e7c416a4ff10
Zineb112 <zbelassiri@gmail.con>
2023-09-05 13:26:47 -0700

    1-repetition_token_0.rb added

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,23 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <input_string>"
-  exit(1)
-end
-
-# Extract the input string from command-line arguments
-input_string = ARGV[0]
-
-# Define the regular expression pattern for matching "School"
-regex_pattern = /School/
-
-# Use the match method to find a match in the input string
-match = input_string.match(regex_pattern)
-
-# Check if a match was found and print the result
-if match
-  puts match[0]
-else
-  puts "No match found."
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
